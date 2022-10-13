199f544c8763f0ebf4adcad40b819bec06c996f5
Dan Clarke <dan@everstack.com>
2022-10-13 07:18:45 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+
+var browser = await playwright.Chromium.LaunchPersistentContextAsync(@"C:\Users\Dan\AppData\Local\Google\Chrome\User Data", new BrowserTypeLaunchPersistentContextOptions
+{
+    Headless = false,
+});
+
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://google.com");
+
+Console.ReadKey();
+
